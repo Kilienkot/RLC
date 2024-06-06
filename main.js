8bcb5ce55df62ac56b46c5426537e260dd905725
Kilienkot <kilienkotim@gmail.com>
2024-06-06 15:08:24 +0300

    mobale_mod_wopop

--- main.js
+++ main.js
@@ -19,3 +19,31 @@ window.addEventListener('scroll', () => {
 
     lastScroll = scrollPosition();
 })
+
+const btns = document.querySelectorAll('.treners__card');
+const modalOverlay = document.querySelector('.modal-overlay ');
+const modals = document.querySelectorAll('.modal');
+
+btns.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let path = e.currentTarget.getAttribute('data-path');
+
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+
+		document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+		modalOverlay.classList.add('modal-overlay--visible');
+	});
+});
+
+modalOverlay.addEventListener('click', (e) => {
+	console.log(e.target);
+
+	if (e.target == modalOverlay) {
+		modalOverlay.classList.remove('modal-overlay--visible');
+		modals.forEach((el) => {
+			el.classList.remove('modal--visible');
+		});
+	}
+});
